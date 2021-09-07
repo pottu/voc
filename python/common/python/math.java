f00cb6c9cde6b15277f9ceee709a3248b4e37c69
Christian Wirén <christian@wiren.cc>
2021-09-07 15:58:03 +0200

    Math.java using __ceil__

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -5,20 +5,18 @@ public class math extends org.python.types.Module {
     @org.python.Attribute
     public static org.python.Object __file__ = new org.python.types.Str("python/common/python/math.java");
     @org.python.Attribute
-    public static org.python.Object __loader__ = org.python.types.NoneType.NONE; // TODO
-    @org.python.Attribute
-    public static org.python.Object __name__ = new org.python.types.Str("time");
-    @org.python.Attribute
-    public static org.python.Object __package__ = new org.python.types.Str("");
-    @org.python.Attribute
-    public static org.python.Object __spec__ = org.python.types.NoneType.NONE; // TODO
+    public static org.python.Object __package__ = new org.python.types.Str("math");
 
     @org.python.Method(__doc__ = "ceil(x) -> number\n\n"
-            + "Return the ceiling of x as an Integral.\n\nThis is the smallest integer >= x.")
+            + "Return the ceiling of x as an Integral.\n\nThis is the smallest integer >= x.", args = { "x" })
     public static org.python.Object ceil(org.python.Object x) {
-        try {
-            return x.__ceil__();
-        } catch (org.python.exceptions.AttributeError ae) {
+        if (x instanceof org.python.types.Float) {
+            org.python.types.Float number = (org.python.types.Float) x;
+            return number.__ceil__();
+        } else if (x instanceof org.python.types.Int) {
+            org.python.types.Int number = (org.python.types.Int) x;
+            return number.__ceil__();
+        } else {
             throw new org.python.exceptions.TypeError("must be real number, not '" + x.typeName() + "'");
         }
     }
--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -9,28 +9,8 @@ class MathModuleTests(TranspileTestCase):
 
     ############################################################
     # ceil
-    @expectedFailure
-    def test_ceil_no_args(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.ceil())
-            """)
-
-    @expectedFailure
-    def test_ceil_string(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.ceil("test"))
-            """)
-
     def test_ceil_float(self):
         self.assertCodeExecution("""
             import math
             print(math.ceil(3.5))
-            """)
-
-    def test_ceil_float(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.ceil(3))
             """)
