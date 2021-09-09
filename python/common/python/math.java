985430863514fc54c294a526ea35feb4ae139694
Pontus Ernstedt <pontus_ponta@hotmail.com>
2021-09-09 14:48:24 +0200

    Implement math.isqrt stdlib method (#1)
    * Initial math.isqrt implementation
    
    * tests for isqrt
    
    * Fix value amd type Error
    
    * Fix flake8 issues
    
    * Adhere to stylechecker
    
    * Print valid uses so that output is actually compared
    
    * Correct documentation
    
    Co-authored-by: sebbersk <seba08_@hotmail.com>

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -1,11 +1,33 @@
 package python;
 
-@org.python.Module(__doc__ = "This module provides access to the mathematical functions\ndefined by the C standard.")
+@org.python.Module(
+        __doc__ =
+        "This module provides access to the mathematical functions\n"
+        + "defined by the C standard.")
+
 public class math extends org.python.types.Module {
+    public math() {
+        super();
+    }
+
     @org.python.Attribute
     public static org.python.Object __file__ = new org.python.types.Str("python/common/python/math.java");
     @org.python.Attribute
-    public static org.python.Object __package__ = new org.python.types.Str("math");
+    public static org.python.Object __name__ = new org.python.types.Str("math");
+
+
+    @org.python.Method(__doc__ = "isqrt(n) -> int\n\n"
+            + "Return the integer part of the square root of the input.",
+            args = { "n" })
+    public static org.python.Object isqrt(org.python.Object n) {
+        if (n instanceof org.python.types.Int) {
+            long n_val = ((org.python.types.Int) n.__int__()).value;
+            if (n_val < 0) {
+                throw new org.python.exceptions.ValueError("isqrt() argument must be nonnegative");
+            }
+            return org.python.types.Int.getInt(((long) Math.floor(Math.sqrt(n_val))));
+        }
+        throw new org.python.exceptions.TypeError("'" + n.typeName() + "' object cannot be interpreted as an integer");
 
     @org.python.Method(__doc__ = "ceil(x) -> number\n\n"
             + "Return the ceiling of x as an Integral.\n\nThis is the smallest integer >= x.", args = { "x" })
--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -12,6 +12,84 @@ class MathModuleTests(TranspileTestCase):
             print(math.__doc__)
             """)
 
+    #######################################################
+    # isqrt
+    @expectedFailure
+    def test_isqrt_string(self):
+        self.assertCodeExecution("""
+            import math
+            math.isqrt('hej')
+            """)
+
+    @expectedFailure
+    def test_isqrt_empty(self):
+        self.assertCodeExecution("""
+            import math
+            math.isqrt()
+            """)
+
+    @expectedFailure
+    def test_isqrt_none(self):
+        self.assertCodeExecution("""
+            import math
+            math.isqrt(None)
+            """)
+
+    @expectedFailure
+    def test_isqrt_negative_int(self):
+        self.assertCodeExecution("""
+            import math
+            math.isqrt(-5)
+            """)
+
+    @expectedFailure
+    def test_isqrt_negative_float(self):
+        self.assertCodeExecution("""
+            import math
+            math.isqrt(-5.8)
+            """)
+
+    @expectedFailure
+    def test_isqrt_positive_float(self):
+        self.assertCodeExecution("""
+            import math
+            math.isqrt(5.8)
+            """)
+
+    def test_isqrt_possible_outcomes(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.isqrt(5))
+            print(math.isqrt(16))
+            print(math.isqrt(0))
+            print(math.isqrt(1))
+            print(math.isqrt(20129310))
+            try:
+                math.isqrt("hej")
+            except Exception as e:
+                print(e)
+            try:
+                math.isqrt(5.5)
+            except Exception as e:
+                print(e)
+            try:
+                math.isqrt([123,52])
+            except Exception as e:
+                print(e)
+            try:
+                math.isqrt(None)
+            except Exception as e:
+                print(e)
+            try:
+                math.isqrt(-5)
+            except Exception as e:
+                print(e)
+            try:
+                math.isqrt((5,2,3))
+            except Exception as e:
+                print(e)
+            """)
+
     ############################################################
     # ceil
     def test_ceil_float(self):
