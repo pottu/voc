b0c8135af5dd72d552d8f7d0442165f4757ccc6f
Pontus Ernstedt <pontus_ponta@hotmail.com>
2021-09-27 16:12:48 +0200

    Updated workload

--- python/workload/Workload.java
+++ python/workload/Workload.java
@@ -2,16 +2,32 @@ package workload;
 
 import org.python.types.List;
 
-
 public class Workload {
-    private static int NUM_ELEMENTS = 100000;
-    public static void main(String[] args) {
+    private static int NUM_RUNS = 1;
+    private static int NUM_ELEMENTS = 210000;
+
+    public static void doWork() {
         List l = new List();
+
         for (int i = 0; i < NUM_ELEMENTS; i++) {
             int rand = (int)(Math.random() * 100 + 1);
-            l.append(org.python.types.Int.getInt(rand));
-            l.sort(null,null);
+            l.insert(org.python.types.Int.getInt(0), org.python.types.Int.getInt(rand));
         }
-        System.out.println("DONE");
+
+        l.sort(null,null);
+
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            l.pop(org.python.types.Int.getInt(0));
+        }
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < NUM_RUNS; i++) {
+            doWork();
+        }
+        long endTime = System.currentTimeMillis();
+        long avgTime = (endTime - startTime) / NUM_RUNS;
+        System.out.println("Time (ms): " + avgTime);
     }
 }
