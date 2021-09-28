6103fb10efc201458f59d54ee02f6fda87ec37d5
Pontus Ernstedt <pontus_ponta@hotmail.com>
2021-09-28 18:25:10 +0200

    Remove unused imports

--- junit/ListTest.java
+++ junit/ListTest.java
@@ -3,12 +3,10 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
-import org.python.stdlib.datetime.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.ArrayList;
